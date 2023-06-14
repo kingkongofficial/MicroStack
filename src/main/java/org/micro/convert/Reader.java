f2b6ee2a0cff9268240a0e1d0385a486889c68cb
krishpranav <krisna.pranav@gmail.com>
2023-06-14 06:23:59 +0530

    micro: convert[Reader]

--- src/main/java/org/micro/convert/Reader.java
+++ src/main/java/org/micro/convert/Reader.java
@@ -4,18 +4,17 @@ import java.util.Map;
 
 public abstract class Reader {
 
-    /**
-     * ValueType
-     */
     public static enum ValueType {
         STRING, ARRAY, MAP;
     }
 
     protected int fieldIndex;
 
-    public static final short SIGN_NULL = 1;
-    public static final short SIGN_NOLENGTH = 2;
-    public static final short SIGN_NOLENBUTBYTES = 3;
+    public static final short SIGN_NULL = -1;
+
+    public static final short SIGN_NOLENGTH = -2;
+
+    public static final short SIGN_NOLENBUTBYTES = -3; 
 
     public abstract void prepare(byte[] content);
 
@@ -27,8 +26,53 @@ public abstract class Reader {
 
     public abstract int position();
 
+    public abstract int readMemberContentLength(DeMember member, Decodeable decoder);
+
     public abstract void skipValue();
 
     public abstract void readBlank();
-    
+
+    public abstract ValueType readType();
+
+    public String readObjectB(final Class clazz) {
+        this.fieldIndex = 0;
+        return null;
+    }
+
+    public abstract void readObjectE(final Class clazz);
+
+    public abstract int readArrayB(DeMember member, byte[] typevals, Decodeable componentDecoder);
+
+    public abstract void readArrayE();
+
+    public abstract int readMapB(DeMember member, byte[] typevals, Decodeable keyDecoder, Decodeable valueDecoder);
+
+    public abstract void readMapE();
+
+    public abstract DeMember readFieldName(final DeMember[] members, Map<String, DeMember> memberFieldMap, Map<Integer, DeMember> memberTagMap);
+
+    public abstract boolean readBoolean();
+
+    public abstract byte readByte();
+
+    public abstract byte[] readByteArray();
+
+    public abstract char readChar();
+
+    public abstract short readShort();
+
+    public abstract int readInt();
+
+    public abstract long readLong();
+
+    public abstract float readFloat();
+
+    public abstract double readDouble();
+
+    public abstract String readSmallString();
+
+    public abstract String readClassName();
+
+    public abstract String readString();
+
 }
