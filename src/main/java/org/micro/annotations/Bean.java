6971188f5e62fd35febdfdf8cf478163644557b9
krishpranav <krisna.pranav@gmail.com>
2023-06-14 05:59:33 +0530

    micro: annotiations[Bean]

---
+++ src/main/java/org/micro/annotations/Bean.java
@@ -0,0 +1,13 @@
+package org.micro.annotations;
+
+import java.lang.annotation.*;
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Inherited
+@Documented
+@Target(TYPE)
+@Retention(RUNTIME)
+public @interface Bean {
+
+}
